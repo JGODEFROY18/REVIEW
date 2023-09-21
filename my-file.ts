6cb7b994f490dcf259bd81960570db4606e2531a
root <root@jgodefroy-23.GROUPE.WAN>
2023-09-21 09:45:37 +0200

    init

---
+++ my-file.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+test('a un titre', async ({ page }) => {
+  await page.goto('http://localhost:5500');
+
+  // S'attend à ce que le titre "contienne" une sous-chaîne.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('lien pour commencer', async ({ page }) => {
+  await page.goto('http://localhost:5500');
+
+  // Clique sur le lien pour commencer.
+  await page.getByRole('button').click();
+
+  // S'attend à ce que la page contienne un titre avec le nom Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
---
+++ tests/example.spec.ts
@@ -0,0 +1,30 @@
+import { chromium, Browser, Page } from 'playwright';
+
+(async () => {
+  // Ouvrir un navigateur Chromium avec Playwright
+  const browser: Browser = await chromium.launch();
+
+  // Créer une nouvelle page
+  const page: Page = await browser.newPage();
+
+  // Accéder à l'URL de votre formulaire HTML
+  await page.goto('http://localhost:5500');
+
+  // Remplir le champ "Nom"
+  await page.fill('#nom', 'John Doe');
+
+  // Remplir le champ "Email"
+  await page.fill('#email', 'jules@email.com');
+
+  // Remplir le champ "Message"
+  await page.fill('#message', 'Ceci est un message de test.');
+
+  // Cliquer sur le bouton "Envoyer"
+  await page.click('button[type=submit]');
+
+  // Attendre un peu pour voir les résultats (facultatif)
+  await page.waitForTimeout(2000); // Attendez 2 secondes, ajustez si nécessaire
+
+  // Fermer le navigateur
+  await browser.close();
+})();
