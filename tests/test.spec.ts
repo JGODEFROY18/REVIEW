6b56fba1f64e570cbbcc61e45b8a9d605d706672
Jules <jules.godefroydu60@gmail.com>
2023-09-22 13:33:36 +0200

    version finale

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,14 +1,17 @@
-import { chromium, Browser, Page } from 'playwright';
+import { chromium, Browser, test } from '@playwright/test';
 
-(async () => {
-  // Ouvrir un navigateur Chromium avec Playwright
-  const browser: Browser = await chromium.launch();
+test.describe('Formulaire', () => {
+  test.beforeEach(async ({ page }) => {
+    // Accéder à l'URL de votre formulaire HTML
+    await page.goto('http://localhost:3000');
+  });
 
-  // Créer une nouvelle page
-  const page: Page = await browser.newPage();
+  test('has title', async ({ page }) => {
+    // Ouvrir un navigateur Chromium avec Playwright
+    const browser: Browser = await chromium.launch();
 
-  // Accéder à l'URL de votre formulaire HTML
-  await page.goto('http://localhost:5500');
+    // Remplir le champ "Nom"
+    await page.fill('#nom', 'John Doe');
 
   // Remplir le champ "Nom"
   await page.fill('#nom', 'John Doe');
@@ -22,9 +25,7 @@ import { chromium, Browser, Page } from 'playwright';
   // Cliquer sur le bouton "Envoyer"
   await page.click('button[type=submit]');
 
-  // Attendre un peu pour voir les résultats (facultatif)
-  await page.waitForTimeout(2000); // Attendez 2 secondes, ajustez si nécessaire
-
-  // Fermer le navigateur
-  await browser.close();
-})();
+    // Fermer le navigateur
+    await browser.close();
+  });
+});
